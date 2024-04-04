5fbc2e26c0ddca672e834316b11faf21e370894a
TommyCat151 <tommycat100@gmail.com>
2024-04-04 18:12:07 +0800

    integrate unit tests with git action yml

--- backend/tests/test_project_index.py
+++ backend/tests/test_project_index.py
@@ -7,28 +7,13 @@ import unittest
 from main import app
 
 class API_test(unittest.TestCase):
-    def test_get_record_index(self):
-        # data = {
-        #     "project_id" : 86,
-        #     "record_id":2
-        # }
-        # data = {
-        #     "project_id" : 86
-        # }
-
+    def test_project_index(self):
         data = {
             "user_id": "1",
             "project_status":"normal"
         }
-
-        # response = self.client.post('/get_record_index', json=data)
-
         response = self.client.post('/project_index', json=data)
-
-        # response = self.client.post('/tag_index', json=data)
-        # response = self.client.post('/get_record', json=data)
-        print(f"\n\nstatus code: {response.status_code}\ndata: {response.data.decode('utf-8')}\n")
-        print(f"headers: {response.headers}")
+        print(response.data.decode('utf-8'))
 
     @classmethod
     def setUpClass(cls):
